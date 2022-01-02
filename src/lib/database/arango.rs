3b58f684c0560aa859bb85ce1e026ca5ed903975
itsezc <chirub@foretag.co>
2022-01-02 12:41:22 +0000

    implement(Arango): create_collection

--- src/lib/database/arango.rs
+++ src/lib/database/arango.rs
@@ -1,6 +1,6 @@
 use std::env::var as ENV_VAR;
 
-use arangors_lite::Connection;
+use arangors_lite::Connection as ArangoConnection;
 use arangors_lite::collection::options::{
 	CreateParameters, 
 	CreateOptions as CollectionOptions
@@ -9,63 +9,72 @@ use arangors_lite::collection::options::{
 use anyhow::Error;
 use serde_json::json;
 
-pub struct Database;
+use arangors_lite::Database as ArangoDatabase;
+
+use crate::lib::schema::types::{ 
+	SchemaDocumentProperty,
+	SchemaRequiredTypes
+};
+
+pub struct Database
+{
+	connection: ArangoConnection,
+	database: ArangoDatabase
+}
 
 impl Database
 {
-	async fn new() -> Result<(), Error>
+	pub async fn new() -> Database
 	{
-		let connection = Connection::establish_basic_auth(
+		let connection = ArangoConnection::establish_basic_auth(
 			ENV_VAR("DB_HOST").unwrap_or(String::from("http://localhost:8529")).as_str(), 
 			ENV_VAR("DB_USER").unwrap_or(String::from("root")).as_str(),
 			ENV_VAR("DB_PASS").unwrap_or(String::from("root01")).as_str(),
-		).await?;
+		)
+		.await
+		.unwrap();
 		
-		let database = connection.db("alchemy").await?;
+		let database = connection
+			.db("alchemy")
+			.await
+			.unwrap();
 
+		// let collections = database.accessible_collections().await?;
+
+		// println!("Collections: {:?}", collections);
+
+		Database
+		{
+			connection,
+			database
+		}
+	}
+
+	pub async fn create_collection(
+		&self, 
+		name: String,
+		properties: SchemaDocumentProperty,
+		required: Option<SchemaRequiredTypes>
+	) -> Result<(), Error> {
 		let schema = json!({
 			"message": "Test validation failed",
 			"level": "strict",
 			"rule": {
 				"type": "object",
-				"properties": {
-					"firstName": {
-						"type": "string",
-						"minLength": 2,
-						"maxLength": 30
-					},
-					"lastName": {
-						"type": "string",
-						"minLength": 2,
-						"maxLength": 30
-					},
-					"role": {
-						"enum": [
-							"lead",
-							"customer",
-						]
-					}
-				}
+				"properties": properties
 			},
-			"required": [
-				"firstName",
-				"lastName"
-			],
+			"required": required.unwrap_or(vec![]),
 			"additionalProperties": false
 		});
 
 		let collection_options = CollectionOptions::builder()
-			.name("test")
+			.name(name.as_str())
 			.schema(schema)
 			.build();
 
-		let test = database.create_collection_with_options(collection_options, CreateParameters::default()).await?;
+		let collection = self.database.create_collection_with_options(collection_options, CreateParameters::default()).await?;
 
-		// println!("Created collection {:?}", test);
-
-		// let collections = database.accessible_collections().await?;
-
-		// println!("Collections: {:?}", collections);
+		println!("Created collection {:?}", collection);
 
 		Ok(())
 	}
