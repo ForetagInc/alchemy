fa75572f0680f39641fda670e21dc64c363d51fa
itsezc <chirub@foretag.co>
2022-01-15 02:04:05 +0000

    implement: Display for property_type schema

--- src/lib/database/schema/property_type.rs
+++ src/lib/database/schema/property_type.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use serde::{ Serialize, Deserialize };
 
 /// The schema property type of the collection
@@ -9,4 +11,19 @@ pub enum SchemaPropertyType
 	Array,
 	Boolean,
 	Enum
+}
+
+impl fmt::Display for SchemaPropertyType
+{
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+	{
+		match self
+		{
+			SchemaPropertyType::String => write!(f, "string"),
+			SchemaPropertyType::Integer => write!(f, "integer"),
+			SchemaPropertyType::Array => write!(f, "array"),
+			SchemaPropertyType::Boolean => write!(f, "boolean"),
+			SchemaPropertyType::Enum => write!(f, "enum"),
+		}
+	}
 }
