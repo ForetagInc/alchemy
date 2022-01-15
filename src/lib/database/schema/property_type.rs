28fa9cc1d4de6e3245d86799734e9777793090d5
itsezc <chirub@foretag.co>
2022-01-15 02:54:39 +0000

    implement: From schema property type

--- src/lib/database/schema/property_type.rs
+++ src/lib/database/schema/property_type.rs
@@ -2,6 +2,8 @@ use core::fmt;
 
 use serde::{ Serialize, Deserialize };
 
+use super::SchemaNativeType;
+
 /// The schema property type of the collection
 #[derive(Serialize, Deserialize, PartialEq, Default)]
 pub enum SchemaPropertyType
@@ -24,6 +26,21 @@ impl fmt::Display for SchemaPropertyType
 			SchemaPropertyType::Array => write!(f, "array"),
 			SchemaPropertyType::Boolean => write!(f, "boolean"),
 			SchemaPropertyType::Enum => write!(f, "enum"),
+		}
+	}
+}
+
+impl From<SchemaPropertyType> for SchemaNativeType
+{
+	fn from(property_type: SchemaPropertyType) -> Self
+	{
+		match property_type
+		{
+			SchemaPropertyType::String => SchemaNativeType::String,
+			SchemaPropertyType::Integer => SchemaNativeType::Integer,
+			SchemaPropertyType::Array => SchemaNativeType::String,
+			SchemaPropertyType::Boolean => SchemaNativeType::Boolean,
+			SchemaPropertyType::Enum => SchemaNativeType::String,
 		}
 	}
 }
