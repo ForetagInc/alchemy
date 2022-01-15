d950a7ba4e731d966b7ba3176908728aab27b4d0
itsezc <chirub@foretag.co>
2022-01-15 02:05:21 +0000

    refactor: schema property struct

--- src/lib/database/schema/property.rs
+++ src/lib/database/schema/property.rs
@@ -1,13 +1,28 @@
 use serde::{ Serialize, Deserialize };
 
-use super::{ SchemaPropertyType, SchemaNativeType };
+use super::SchemaNativeTypeArray;
 
 /// The schema property
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Default)]
 pub struct SchemaProperty
 {
-	pub r#type: SchemaPropertyType,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub r#type: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub min_length: Option<i32>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub max_length: Option<i32>,
-	pub items: Option<Vec<SchemaNativeType>>
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub r#enum: Option<Vec<String>>,
+	/// Only set if the type is an Array
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub items: Option<SchemaNativeTypeArray>
+}
+
+impl SchemaProperty
+{
+	pub fn new() -> Self
+	{
+		Default::default()
+	}
 }
