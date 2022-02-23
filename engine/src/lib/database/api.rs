3880da96ba782a551757fa14fa6578815cda5d42
KennethGomez <kennethgomezfdz@gmail.com>
2022-02-23 01:33:25 +0100

    implement: GraphQL Scalar types from Json types

--- engine/src/lib/database/api.rs
+++ engine/src/lib/database/api.rs
@@ -1,5 +1,96 @@
 use crate::lib::schema::get_all_entries;
 
+struct GraphQLProperty {
+	name: String,
+	scalar_type: ScalarType
+}
+
+struct ScalarType {
+	raw: RawScalarType,
+	child: Option<RawScalarType>
+}
+
+#[derive(PartialEq)]
+pub enum RawScalarType {
+	Array,
+	Nullable,
+	String,
+	Object,
+	Float,
+	Int,
+	Boolean,
+	Enum,
+}
+
+impl From<JsonType> for ScalarType {
+	fn from(json_type: JsonType) -> Self {
+		match json_type.raw {
+			RawJsonType::Array => Self {
+				raw: json_type.raw.into(),
+				child: Some(json_type.child.expect("ERROR: Array child type not defined").into())
+			},
+			_ => Self {
+				raw: json_type.raw.into(),
+				child: None
+			}
+		}
+	}
+}
+
+impl From<RawJsonType> for RawScalarType {
+	fn from(raw_type: RawJsonType) -> Self {
+		match raw_type {
+			RawJsonType::Array => RawScalarType::Array,
+			RawJsonType::Boolean => RawScalarType::Boolean,
+			RawJsonType::Integer => RawScalarType::Int,
+			RawJsonType::Number => RawScalarType::Float,
+			RawJsonType::Object => RawScalarType::Object,
+			RawJsonType::String => RawScalarType::String,
+			RawJsonType::Nullable => RawScalarType::Nullable
+		}
+	}
+}
+
+struct JsonType {
+	raw: RawJsonType,
+	child: Option<RawJsonType>
+}
+
+pub enum RawJsonType {
+	Array,
+	Boolean,
+	Nullable,
+	Integer,
+	Number,
+	Object,
+	String,
+}
+
+impl From<&str> for RawJsonType {
+	fn from(json_type: &str) -> Self {
+		match json_type {
+			"array" => Self::Array,
+			"boolean" => Self::Boolean,
+			"integer" => Self::Integer,
+			"number" => Self::Number,
+			"object" => Self::Object,
+			"string" => Self::String,
+			_ => Self::Nullable
+		}
+	}
+}
+
+impl From<&str> for JsonType {
+	fn from(s: &str) -> Self {
+		match s {
+			_ => Self {
+				raw: s.into(),
+				child: None
+			}
+		}
+	}
+}
+
 pub async fn generate_sdl()
 {
 	let entries = get_all_entries().await;
@@ -13,25 +104,29 @@ pub async fn generate_sdl()
 		let name = &entry["name"].as_str().unwrap();
 		let entry_properties = &entry["schema"].get("properties").unwrap();
 
-		let mut props = String::new();
+		let mut props: Vec<GraphQLProperty> = Vec::new();
 
 		for prop in entry_properties.as_object().unwrap().iter()
 		{
-			let prop_name = &prop.0;
-			let prop_type = &prop.1["type"].as_str().unwrap();
+			let prop_name = prop.0.clone();
 
-			props.push_str(&format!("\t{}: {}\n", prop_name, prop_type));
+			let json_type: JsonType = prop.1["type"].as_str().unwrap().into();
+			let scalar_type: ScalarType = json_type.into();
+
+			props.push(GraphQLProperty {
+				name: prop_name,
+				scalar_type
+			});
 		}
-
-		println!("Props of {} are: {}", name, props);
 
 		let object = format!(
 			r#"
 				type {} {{
-					test
+					{}
 				}}
 			"#,
-			format!("{}{}", (&name[..1].to_string()).to_uppercase(), &name[1..])
+			format!("{}{}", (&name[..1].to_string()).to_uppercase(), &name[1..]),
+			get_type_body(props)
 		);
 
 		sdl.push_str(&object);
@@ -40,4 +135,33 @@ pub async fn generate_sdl()
 
 	println!("----- SDL GENERATED -----");
 	println!("SDL: {}", sdl);
+}
+
+fn get_type_body(props: Vec<GraphQLProperty>) -> String {
+	let mut body = String::new();
+
+	for prop in props {
+		body.push_str(format!("{}: {}\n", prop.name, parse_graphql_prop_type(prop.scalar_type)).as_str())
+	}
+
+	body
+}
+
+fn parse_graphql_prop_type(prop_type: ScalarType) -> String {
+	match prop_type.raw {
+		RawScalarType::String => "String",
+		RawScalarType::Object => "String",
+		RawScalarType::Float => "Float",
+		RawScalarType::Int => "Int",
+		RawScalarType::Boolean => "Boolean",
+		RawScalarType::Array => {
+			"array"
+		}
+		RawScalarType::Nullable => {
+			"null"
+		}
+		RawScalarType::Enum => {
+			"enum"
+		}
+	}.to_string()
 }
