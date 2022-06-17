26770b948f960d6301a121f3a77f88f5a4cb515d
KennethGomez <kennethgomezfdz@gmail.com>
2022-06-17 13:36:03 +0200

    implement: Complete insert entity type with relationships

--- engine/src/api/schema/input/insert.rs
+++ engine/src/api/schema/input/insert.rs
@@ -63,38 +63,17 @@ where
 	where
 		S: 'r,
 	{
-		let mut args = Vec::new();
-
-		for property in &info.data.entity.properties {
-			if property.name.eq("_key") {
-				continue;
-			}
-
-			let arg = build_argument_from_property(
-				registry,
-				&property,
-				&property.scalar_type,
-				property.required,
-			);
-
-			args.push(arg);
-		}
-
-		for relationship in &*info.data.relationships {
-			let rel_data = &info
-				.registry
-				.get_operation_data(&relationship.to.name)
-				.expect("Relationship entity operation data not found");
-
-			let rel_info = &EntityRelationshipInsertData::new(rel_data, &info.registry);
-
-			let arg = build_insert_arg_from_relationship(registry, relationship, rel_info);
-
-			args.push(arg);
-		}
+		let attributes = registry.arg::<EntityAttributesInsert>(
+			"attributes",
+			&EntityAttributesInsertData::new(info.data, info.registry),
+		);
+		let relationships = registry.arg::<Option<EntityRelationshipsInsert>>(
+			"relationships",
+			&EntityRelationshipsInsertData::new(info.data, info.registry),
+		);
 
 		registry
-			.build_input_object_type::<EntityInsert>(info, &args)
+			.build_input_object_type::<EntityInsert>(info, &vec![attributes, relationships])
 			.into_meta()
 	}
 }
@@ -112,6 +91,180 @@ where
 }
 
 impl<'a, S> FromInputValue<S> for EntityInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	fn from_input_value(data: &InputValue<S>) -> Option<Self> {
+		Some(Self {
+			data: input_value_to_string(data),
+
+			_marker: Default::default(),
+		})
+	}
+}
+
+pub struct EntityAttributesInsert<'a> {
+	pub data: String,
+
+	_marker: PhantomData<&'a ()>,
+}
+
+pub struct EntityAttributesInsertData<'a, S>
+where
+	S: AsyncScalarValue,
+{
+	pub name: String,
+	pub data: &'a OperationData<S>,
+	pub registry: &'a OperationRegistry<S>,
+}
+
+impl<'a, S> EntityAttributesInsertData<'a, S>
+where
+	S: AsyncScalarValue,
+{
+	pub fn new(data: &'a OperationData<S>, registry: &'a OperationRegistry<S>) -> Self {
+		Self {
+			name: format!("{}AttributesInsert", data.entity.name.as_str()),
+			data,
+			registry,
+		}
+	}
+}
+
+impl<'a, S> GraphQLType<S> for EntityAttributesInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	fn name(info: &Self::TypeInfo) -> Option<&str> {
+		Some(info.name.as_str())
+	}
+
+	fn meta<'r>(info: &Self::TypeInfo, registry: &mut Registry<'r, S>) -> MetaType<'r, S>
+	where
+		S: 'r,
+	{
+		let mut args = Vec::new();
+
+		for property in &info.data.entity.properties {
+			if property.name.eq("_key") {
+				continue;
+			}
+
+			let arg = build_argument_from_property(
+				registry,
+				&property,
+				&property.scalar_type,
+				property.required,
+			);
+
+			args.push(arg);
+		}
+
+		registry
+			.build_input_object_type::<EntityAttributesInsert>(info, &args)
+			.into_meta()
+	}
+}
+
+impl<'a, S> GraphQLValue<S> for EntityAttributesInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	type Context = ();
+	type TypeInfo = EntityAttributesInsertData<'a, S>;
+
+	fn type_name<'i>(&self, info: &'i Self::TypeInfo) -> Option<&'i str> {
+		<Self as GraphQLType<S>>::name(info)
+	}
+}
+
+impl<'a, S> FromInputValue<S> for EntityAttributesInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	fn from_input_value(data: &InputValue<S>) -> Option<Self> {
+		Some(Self {
+			data: input_value_to_string(data),
+
+			_marker: Default::default(),
+		})
+	}
+}
+
+pub struct EntityRelationshipsInsert<'a> {
+	pub data: String,
+
+	_marker: PhantomData<&'a ()>,
+}
+
+pub struct EntityRelationshipsInsertData<'a, S>
+where
+	S: AsyncScalarValue,
+{
+	pub name: String,
+	pub data: &'a OperationData<S>,
+	pub registry: &'a OperationRegistry<S>,
+}
+
+impl<'a, S> EntityRelationshipsInsertData<'a, S>
+where
+	S: AsyncScalarValue,
+{
+	pub fn new(data: &'a OperationData<S>, registry: &'a OperationRegistry<S>) -> Self {
+		Self {
+			name: format!("{}RelationshipsInsert", data.entity.name.as_str()),
+			data,
+			registry,
+		}
+	}
+}
+
+impl<'a, S> GraphQLType<S> for EntityRelationshipsInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	fn name(info: &Self::TypeInfo) -> Option<&str> {
+		Some(info.name.as_str())
+	}
+
+	fn meta<'r>(info: &Self::TypeInfo, registry: &mut Registry<'r, S>) -> MetaType<'r, S>
+	where
+		S: 'r,
+	{
+		let mut args = Vec::new();
+
+		for relationship in &*info.data.relationships {
+			let rel_data = &info
+				.registry
+				.get_operation_data(&relationship.to.name)
+				.expect("Relationship entity operation data not found");
+
+			let rel_info = &EntityRelationshipInsertData::new(rel_data, &info.registry);
+
+			let arg = build_insert_arg_from_relationship(registry, relationship, rel_info);
+
+			args.push(arg);
+		}
+
+		registry
+			.build_input_object_type::<EntityRelationshipsInsert>(info, &args)
+			.into_meta()
+	}
+}
+
+impl<'a, S> GraphQLValue<S> for EntityRelationshipsInsert<'a>
+where
+	S: AsyncScalarValue,
+{
+	type Context = ();
+	type TypeInfo = EntityRelationshipsInsertData<'a, S>;
+
+	fn type_name<'i>(&self, info: &'i Self::TypeInfo) -> Option<&'i str> {
+		<Self as GraphQLType<S>>::name(info)
+	}
+}
+
+impl<'a, S> FromInputValue<S> for EntityRelationshipsInsert<'a>
 where
 	S: AsyncScalarValue,
 {
@@ -164,9 +317,10 @@ where
 	where
 		S: 'r,
 	{
-		let insert_data = &EntityInsertData::new(info.data, info.registry);
-
-		let new = registry.arg::<Option<EntityInsert>>("addNew", insert_data);
+		let new = registry.arg::<Option<EntityAttributesInsert>>(
+			"addNew",
+			&EntityAttributesInsertData::new(info.data, info.registry),
+		);
 		let existing = registry.arg::<Option<EntityIndicesFilter<S>>>(
 			"addExisting",
 			&EntityIndicesFilterData::new(info.data),
